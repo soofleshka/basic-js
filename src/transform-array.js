651ce88cb87702a71dbf9f830e6e2e60a4113c6e
Artem Akasevich <arthouse92@gmail.com>
2021-09-25 04:20:28 +0300

    feat: solve tasks feat: solve hanoi-tower feat: solve recursive-depth feat: solve transform-array feat: solve vigenere-cipher

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,17 +3,73 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+export default function transform(arr) {
+  if (!Array.isArray(arr))
+    throw new Error("'arr' parameter must be an instance of the Array!");
+
+  let newArr = [...arr];
+  for (let i = 0; i < newArr.length; i++) {
+    if (isControlSequence(newArr[i]))
+      newArr = applyControlSequence(newArr[i], i, newArr);
+  }
+  return newArr.filter((item) => item);
+}
+
+function isControlSequence(arrElem) {
+  return /^--/.test(arrElem);
+}
+
+function applyControlSequence(sequence, arrIndex, newArr) {
+  switch (sequence) {
+    case '--discard-next':
+      if (arrIndex === newArr.length - 1) newArr = newArr.slice(0, arrIndex);
+      else
+        newArr = [
+          ...newArr.slice(0, arrIndex),
+          undefined,
+          ...newArr.slice(arrIndex + 2),
+        ];
+      break;
+    case '--discard-prev':
+      if (arrIndex === 0) newArr = newArr.slice(1);
+      else
+        newArr = [
+          ...newArr.slice(0, arrIndex - 1),
+          undefined,
+          ...newArr.slice(arrIndex + 1),
+        ];
+      break;
+    case '--double-next':
+      if (arrIndex === newArr.length - 1) newArr = newArr.slice(0, arrIndex);
+      else
+        newArr = [
+          ...newArr.slice(0, arrIndex),
+          newArr[arrIndex + 1],
+          ...newArr.slice(arrIndex + 1),
+        ];
+      break;
+    case '--double-prev':
+      if (arrIndex === 0) newArr = newArr.slice(1);
+      else
+        newArr = [
+          ...newArr.slice(0, arrIndex),
+          newArr[arrIndex - 1],
+          ...newArr.slice(arrIndex + 1),
+        ];
+      break;
+    default:
+      break;
+  }
+  return newArr;
 }
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,29 +3,75 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  constructor() {
+    this.isReverse = arguments[0] === false;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    return this._work(message, key, 'encrypt');
+  }
+  decrypt(encryptedMessage, key) {
+    return this._work(encryptedMessage, key, 'decrypt');
+  }
+
+  _work(m, k, mode) {
+    if (
+      m === undefined ||
+      m !== String(m) ||
+      k === undefined ||
+      k !== String(k)
+    )
+      throw new Error('Incorrect arguments!');
+
+    m = m.toUpperCase();
+    k = k.toUpperCase();
+    let result = '';
+    let keywordIndex = 0;
+    const n = this._alphabet.length;
+
+    m.split('').forEach((char) => {
+      if (!this._alphabet.includes(char)) {
+        result += char;
+        return;
+      }
+
+      let c;
+      if (mode === 'encrypt')
+        c =
+          (this._alphabet.indexOf(char) +
+            this._alphabet.indexOf(k[keywordIndex])) %
+          n;
+      else {
+        c =
+          (this._alphabet.indexOf(char) -
+            this._alphabet.indexOf(k[keywordIndex])) %
+          n;
+      }
+
+      result += [...this._alphabet].splice(c, 1);
+      ++keywordIndex;
+      if (keywordIndex === k.length) keywordIndex = 0;
+    });
+
+    if (this.isReverse) return result.split('').reverse().join('');
+    return result;
   }
 }
